61345b99dc98b99714d437769140b90355a7e61d
VitoriaNauanda <cantao162@gmail.com>
2023-08-01 10:43:26 -0300

    correcoes

--- Curso-Flutter/semana2/dia1/projeto_flutter_2023_d1/lib/main.dart
+++ Curso-Flutter/semana2/dia1/projeto_flutter_2023_d1/lib/main.dart
@@ -7,61 +7,61 @@ void main() {
       'Mousse de Chocolate',
       'Pudim de Leite Condensado'
     ],
-    'Pratos Principais':[
+    'Pratos Principais': [
       'Frango assado com Batatas',
       'Espaguete a Bolonhesa',
       'Risoto de Cogumelos'
     ],
-    'Aperitivos':[
+    'Aperitivos': [
       'Bolinhos de Queijo',
       'Bruschetta de Tomate e Majericão',
       'Canapés de Salmão com Cream Cheese'
     ]
   };
 
-  runApp( MainApp(dados: dados, filtro: 1,));
+  runApp(MainApp(
+    dados: dados,
+    filtro: 1,
+  ));
 }
 
 class MainApp extends StatelessWidget {
   Map<String, List<String>> dados;
   int filtro;
-  MainApp({super.key, required Map<String, List<String>> this.dados, required this.filtro});
+  MainApp(
+      {super.key,
+      required Map<String, List<String>> this.dados,
+      required this.filtro});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primaryColor: Colors.blue,
+        theme: ThemeData(
+          primaryColor: Colors.blue,
         ),
-      home: Scaffold(
-        appBar: AppBar(title: Text("Minhas Receitas")),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(child:  Text(dados.keys.toList()[0], style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),),),
-            Column(
-              children: [
-                Text(dados['Sobremesas']![0]),
-                Text(dados['Sobremesas']![1]),
-                Text(dados['Sobremesas']![2]),
-              ],),
-            Center(child:  Text(dados.keys.toList()[1], style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),),),
-            Column(
-              children: [
-                Text(dados['Pratos Principais']![0]),
-                Text(dados['Pratos Principais']![1]),
-                Text(dados['Pratos Principais']![2]),
-              ],),
-            Center(child:  Text(dados.keys.toList()[2], style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),),),
-            Column(
-              children: [
-                Text(dados['Aperitivos']![0]),
-                Text(dados['Aperitivos']![1]),
-                Text(dados['Aperitivos']![2]),
-              ],),
-          ],),
-        floatingActionButton: FloatingActionButton(child: Icon(Icons.search), onPressed: () => print("Add"),),
-      ),
-    );
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Minhas Receitas"),
+            centerTitle: true,
+          ),
+          body: Column(
+            children: dados.entries
+                .map((categoria) => Column(
+                      children: [
+                        Text(
+                          categoria.key,
+                          style: TextStyle(
+                              fontSize: 24, fontWeight: FontWeight.bold),
+                        ),
+                        for (String value in categoria.value)
+                          Text(
+                            value,
+                            style: TextStyle(fontSize: 18),
+                          )
+                      ],
+                    ))
+                .toList(),
+          ),
+        ));
   }
 }
