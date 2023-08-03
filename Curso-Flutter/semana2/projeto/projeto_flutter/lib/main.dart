767ccbaad2a2cfd8219e8d8b81f310fa66758d94
VitoriaNauanda <cantao162@gmail.com>
2023-08-03 11:35:40 -0300

    stateless e stateful

--- Curso-Flutter/semana2/projeto/projeto_flutter/lib/main.dart
+++ Curso-Flutter/semana2/projeto/projeto_flutter/lib/main.dart
@@ -1,53 +1,78 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
 void main() {
-  runApp(MainApp());
+  runApp(MaterialApp(
+    home:Tela(),
+  ));
 }
 
-class MainApp extends StatelessWidget {
-  MainApp({super.key});
+class Tela extends StatelessWidget {
+  @override
+  Widget build (BuildContext context){
+    return Scaffold(
+    body: Column(children: [ const Cabecalho(), const Corpo(), Contador(valorInicial: 3)]));
+  }
+}
 
-  final Map<String, List<String>> dados = {
-    'Sobremesas': [
-      'Torta de Maçã',
-      'Mousse de Chocolate',
-      'Pudim de Leite de Condensado'
-    ],
-    'Pratos Principais': ['Frango Assado com Batatas', 'Espaguete', 'Risoto'],
-    'Aperitivos': ['Bolinhos de Queijo', 'Bruschetta', 'Canapés']
-  };
+class Cabecalho extends StatefulWidget {
+  const Cabecalho({super.key});
+  
+  @override
+  State<Cabecalho> createState() => _CabecalhoState();
+}
 
-  final String? categoriaUsuario = null;
-  final String busca = '';
-
+class _CabecalhoState extends State<Cabecalho> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(title: const Text("Receitas")),
-          body: Center(
-            child: SizedBox(
-              height: MediaQuery.of(context).size.height * 0.8,
-              width: MediaQuery.of(context).size.width * 0.8,
-              child: Column(
-                  children: dados.entries.map((categoria) {
-                return Container(
-                    margin: const EdgeInsets.all(10.0),
-                    color: Colors.amber,
-                    child: Column(
-                      children: [
-                        Text(categoria.key, style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-                        Row(
-                          children: categoria.value
-                              .map((prato) => Expanded(child: Text(prato)))
-                              .toList(),
-                        )
-                      ],
-                    ));
-              }).toList()),
-            ),
-          )),
+    return Text("Cabecalho");
+  }
+}
+
+class Corpo extends StatefulWidget {
+  final int valor = 10;
+  const Corpo({super.key});
+  
+  @override
+  State<Corpo> createState() => _CorpoState();
+  
+}
+
+class _CorpoState extends State<Corpo> {
+  @override
+  Widget build (BuildContext context){
+    return Text('valor ${widget.valor}');
+  }
+}
+
+class Contador extends StatefulWidget {
+  final int valorInicial;
+  Contador({super.key, required this.valorInicial});
+  
+  @override
+  State<Contador> createState() => _ContadorState();
+}
+
+class _ContadorState extends State<Contador> {
+  late int count = 0;
+  @override
+  void initState() {
+    super.initState();
+    count = widget.valorInicial;
+  }
+  @override
+  Widget build(BuildContext context){
+    return Column(
+      children: [
+        Text("$count"),
+        TextButton(
+          child: const Text('+'),
+          onPressed: () {
+            setState(() {
+              count += 1;
+            });
+          }
+        )
+      ]
     );
   }
 }
