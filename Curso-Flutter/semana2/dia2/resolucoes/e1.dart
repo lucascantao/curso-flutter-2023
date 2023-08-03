dd22b45690b9135b42f7848dda36e795fa2294ab
lucas cantao <cantao162@gmail.com>
2023-08-03 16:12:59 -0300

    S2D2e1

---
+++ Curso-Flutter/semana2/dia2/resolucoes/e1.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+void main() {
+  runApp(MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  MainApp({super.key});
+
+  final Map<String, List<String>> dados = {
+    'Sobremesas': [
+      'Torta de Maçã',
+      'Mousse de Chocolate',
+      'Pudim de Leite de Condensado'
+    ],
+    'Pratos Principais': ['Frango Assado com Batatas', 'Espaguete', 'Risoto'],
+    'Aperitivos': ['Bolinhos de Queijo', 'Bruschetta', 'Canapés']
+  };
+
+  final String? categoriaUsuario = null;
+  final String busca = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(title: const Text("Aula de Container")),
+            body: Center(
+                child: Container(
+              alignment: Alignment.center,
+              height: 500,
+              width: 500,
+              decoration: BoxDecoration(
+                  color: Colors.blueGrey,
+                  borderRadius: BorderRadius.circular(5)),
+              child: const Text(
+                "Um Container",
+                style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+            ))));
+  }
+}
