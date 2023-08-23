9bcb4637bf4b59ef477b5ff1c48012a5fe2fb01c
lucascantao <cantao162@gmail.com>
2023-08-23 12:08:18 -0300

    Sliver scroll

--- curso-especifico/portfolio/lib/main.dart
+++ curso-especifico/portfolio/lib/main.dart
@@ -16,71 +16,84 @@ class MainApp extends StatelessWidget {
   const MainApp({super.key});
   @override
   Widget build(BuildContext context) {
-    GlobalKey keySection1 = GlobalKey();
-    GlobalKey keySection2 = GlobalKey();
-    GlobalKey keySection3 = GlobalKey();
-    GlobalKey keySection4 = GlobalKey();
+    ScrollController scrollController = ScrollController();
+    const List<Widget> sections = [
+      Resume(),
+      DoneProjects(),
+      SkillKnowledge(),
+      About(),
+      Footer()
+    ];
+    // GlobalKey keySection1 = GlobalKey();
+    // GlobalKey keySection2 = GlobalKey();
+    // GlobalKey keySection3 = GlobalKey();
+    // GlobalKey keySection4 = GlobalKey();
     return MaterialApp(
       theme: ThemeData.dark(),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            "Portfolio",
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          toolbarHeight: MediaQuery.of(context).size.height * 0.1,
-          actions: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextButton(
-                onPressed: () async {
-                  await Scrollable.ensureVisible(keySection1.currentContext!,
-                      duration: const Duration(milliseconds: 600));
-                },
-                child: const Text("Resume"),
-              ),
+        // appBar: AppBar(
+        //   title: const Text(
+        //     "Portfolio",
+        //     style: TextStyle(fontWeight: FontWeight.bold),
+        //   ),
+        //   toolbarHeight: MediaQuery.of(context).size.height * 0.1,
+        //   actions: [
+        //     Padding(
+        //       padding: const EdgeInsets.all(8.0),
+        //       child: TextButton(
+        //         onPressed: () async {
+        //           await Scrollable.ensureVisible(keySection1.currentContext!,
+        //               duration: const Duration(milliseconds: 600));
+        //         },
+        //         child: const Text("Resume"),
+        //       ),
+        //     ),
+        //     Padding(
+        //       padding: const EdgeInsets.all(8.0),
+        //       child: TextButton(
+        //         onPressed: () async {
+        //           await Scrollable.ensureVisible(keySection2.currentContext!,
+        //               duration: const Duration(milliseconds: 600));
+        //         },
+        //         child: const Text("Done Projects"),
+        //       ),
+        //     ),
+        //     Padding(
+        //       padding: const EdgeInsets.all(8.0),
+        //       child: TextButton(
+        //         onPressed: () async {
+        //           await Scrollable.ensureVisible(keySection3.currentContext!,
+        //               duration: const Duration(milliseconds: 600));
+        //         },
+        //         child: const Text("Skill"),
+        //       ),
+        //     ),
+        //     Padding(
+        //       padding: const EdgeInsets.all(8.0),
+        //       child: TextButton(
+        //         onPressed: () async {
+        //           await Scrollable.ensureVisible(keySection4.currentContext!,
+        //               duration: const Duration(milliseconds: 600));
+        //         },
+        //         child: const Text("About"),
+        //       ),
+        //     ),
+        //   ],
+        // ),
+        body: CustomScrollView(
+          physics: const BouncingScrollPhysics(),
+          controller: scrollController,
+          slivers: <Widget>[
+            const SliverAppBar(
+              title: Text("Portfolio"),
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextButton(
-                onPressed: () async {
-                  await Scrollable.ensureVisible(keySection2.currentContext!,
-                      duration: const Duration(milliseconds: 600));
-                },
-                child: const Text("Done Projects"),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextButton(
-                onPressed: () async {
-                  await Scrollable.ensureVisible(keySection3.currentContext!,
-                      duration: const Duration(milliseconds: 600));
-                },
-                child: const Text("Skill"),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextButton(
-                onPressed: () async {
-                  await Scrollable.ensureVisible(keySection4.currentContext!,
-                      duration: const Duration(milliseconds: 600));
-                },
-                child: const Text("About"),
-              ),
-            ),
+            SliverList(
+                delegate: SliverChildBuilderDelegate(
+                    (BuildContext context, int index) {
+              return Container(child: sections[index]);
+            }, childCount: sections.length))
           ],
-        ),
-        body: SingleChildScrollView(
-          child: Column(children: [
-            Resume(key: keySection1),
-            DoneProjects(key: keySection2),
-            SkillKnowledge(key: keySection3),
-            About(key: keySection4),
-            const Footer()
-          ]),
         ),
       ),
     );
